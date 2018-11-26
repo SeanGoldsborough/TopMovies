be625df39c9b125d5bc9179802d1b74e7462827c
Sean Goldsborough <smgoldsborough@gmail.com>
2018-11-26 09:21:11 -0500

    test: adds unit test files/targets

---
+++ TopMoviesTests/SettingsTests.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsTests.swift
+//  TopMoviesTests
+//
+//  Created by Sean Goldsborough on 11/26/18.
+//  Copyright © 2018 Sean Goldsborough. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import TopMovies
+
+class SettingsTests: XCTestCase {
+    
+    
+    
+    
+    
+}
