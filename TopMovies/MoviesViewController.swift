d330b809b3e061cbc6f4fa3e29fbfecd383aa3a7
Sean Goldsborough <smgoldsborough@gmail.com>
2018-12-27 16:21:13 -0500

    feat: adds movieVC

---
+++ TopMovies/MoviesViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesViewController.swift
+//  TopMovies
+//
+//  Created by Sean Goldsborough on 12/27/18.
+//  Copyright © 2018 Sean Goldsborough. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class MoviesViewController: UIViewController {
+}
---
+++ TopMoviesTests/MoviesViewControllerTests.swift
@@ -0,0 +1,23 @@
+//
+//  MoviesViewControllerTests.swift
+//  TopMoviesTests
+//
+//  Created by Sean Goldsborough on 12/27/18.
+//  Copyright © 2018 Sean Goldsborough. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import XCTest
+
+class MoviesViewControllerTests: XCTestCase {
+
+    func moviesViewController() -> MoviesViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(
+            withIdentifier: "MoviesViewController")
+            as! MoviesViewController
+        let _ = vc.view
+        return vc }
+
+}
